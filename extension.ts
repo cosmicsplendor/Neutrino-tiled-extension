60eb2e04d3e425eb2f72ff727a620b811302c63c
unknown <ufocalypse@gmail.com>
2021-07-11 16:27:00 +0545

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -10,7 +10,11 @@ const neutrinoFormatExtension = {
             height: map.height * tileHeight,
             collisionRects: [],
             spawnPoints: [],
-            tiles: []
+            markerPoints: [],
+            tiles: [],
+            fgTiles: [],
+            bgTiles: [], // bg tiles
+            fbgTiles: [] // far bg tiles
         }
         const tiles = map.tilesets[0].tiles
         const layers = Array(map.layerCount).fill(0).map((_, i) => i)
@@ -21,14 +25,17 @@ const neutrinoFormatExtension = {
                     case "collision":  // collision layer
                         layer.objects.forEach(obj => {
                             const { x, y , width, height } = obj
-                            customMap.collisionRects.push({ x, y, width, height })
+                            customMap.collisionRects.push({ x: Math.round(x), y: Math.round(y), width: Math.round(width), height: Math.round(height) })
                         })
                     break
                     case "spawn-points": // spawn points layer
                         layer.objects.forEach(({ name, x, y }) => {
                             if (!name) { throw new Error("Anonymous spawn-point")}
-                            customMap.spawnPoints.push({ name, x, y })
+                            customMap.spawnPoints.push({ name, x: Math.round(x), y: Math.round(y) })
                         })
+                    break
+                    case "marker-points":
+                        // marker points
                     break
                 }
                 return
@@ -36,6 +43,10 @@ const neutrinoFormatExtension = {
             // map layer
             const { width: mapWidth, height: mapHeight } = layer
             const tilemap = Array(mapWidth * mapHeight).fill(0).map((_, i) => i)
+            const tilesArray = (layer.name === "foreground" && customMap.fgTiles) || 
+                                (layer.name === "background" && customMap.bgTiles) ||    
+                                (layer.name === "far-background" && customMap.fbgTiles) ||    
+                                customMap.tiles
             tilemap.forEach(i => {
                 const column = i % mapWidth
                 const row = Math.floor(i / mapWidth)
@@ -49,7 +60,7 @@ const neutrinoFormatExtension = {
                     y: (row + 1) * tileHeight - tileImageHeight, // computing the correct y value by taking offsets into account (changing it from y-coordinates of the tile at bottom-left corner of tileImage to the y-coordinates of top left-corner of the image) 
                     name: tileName || altTileName
                 }
-                customMap.tiles.push(tile)
+                tilesArray.push(tile)
             })
         })
         const outputFilename = fileName.replace(/\..+$/, ".cson") // making sure the output file extension is cson
