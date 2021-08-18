38040b01e74e96a2e379105f2e316d3c15288d58
unknown <ufocalypse@gmail.com>
2021-08-18 19:41:39 +0545

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -11,8 +11,9 @@ const neutrinoFormatExtension = {
             collisionRects: [],
             spawnPoints: [],
             markerPoints: [],
-            tiles: [],
-            fgTiles: [],
+            fgTiles: [], // foreground tiles
+            tiles: [], // world tiles
+            mgTiles: [], // midground tiles
             bgTiles: [], // bg tiles
             fbgTiles: [] // far bg tiles
         }
@@ -54,6 +55,7 @@ const neutrinoFormatExtension = {
             const { width: mapWidth, height: mapHeight } = layer
             const tilemap = Array(mapWidth * mapHeight).fill(0).map((_, i) => i)
             const tilesArray = (layer.name === "foreground" && customMap.fgTiles) || 
+                                (layer.name === "midground" && customMap.mgTiles) ||
                                 (layer.name === "background" && customMap.bgTiles) ||    
                                 (layer.name === "far-background" && customMap.fbgTiles) ||    
                                 customMap.tiles
