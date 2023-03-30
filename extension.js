fceaa8ddc6b7dca90f6c9e7f7cbaab50e5dd65a2
unknown <ufocalypse@gmail.com>
2023-03-30 11:08:06 +0545

    Update the extension so it can export flexible number of layers

--- extension.js
+++ extension.js
@@ -3,20 +3,14 @@ const neutrinoFormatExtension = {
     extension: "cson",
     write(map, fileName) {
         const { tileWidth, tileHeight } = map
-        const customMap = {
+        const customMapData = {
             width: map.width * tileWidth,
             height: map.height * tileHeight,
             collisionRects: [],
-            spawnPoints: [],
-            fgTiles: [], // foreground tiles
-            tiles: [], // world tiles
-            mgTiles: [], // midground tiles
-            bgTiles: [], // bg tiles
-            fbgTiles: [], // far bg tiles
-            checkpoints: []
+            checkpoints: [],
+            layers: [],
         }
-        Object.assign(customMap, map.properties())
-        // const tiles = map.tilesets[0].tiles
+        Object.assign(customMapData, map.properties())
         const tilesByTileset = map.tilesets.reduce((acc, cur) => {
             acc[cur.name] = cur.tiles
             return acc
@@ -24,46 +18,49 @@ const neutrinoFormatExtension = {
         const layers = Array(map.layerCount).fill(0).map((_, i) => i)
         layers.forEach(layerIndex => {
             const layer = map.layerAt(layerIndex)
-            if (layer.isObjectLayer) {
-                switch(layer.name) {
-                    case "collision":  // collision layer
-                        layer.objects.forEach(obj => {
-                            const { x, y , width, height, properties } = obj
-                            customMap.collisionRects.push(Object.assign(
-                                { x: Math.round(x), y: Math.round(y), width: Math.round(width), height: Math.round(height) },
-                                properties()
-                            ))
-                        })
-                    break
-                    case "spawn-points": // spawn points layer
-                        layer.objects.forEach(({ name, x, y, properties }) => {
-                            if (!name) { throw new Error("Anonymous spawn-point")}
-                            customMap.spawnPoints.push(Object.assign(
-                                { name, x: Math.round(x), y: Math.round(y) },
-                                properties()
-                            ))
-                        })
-                    break
-                    case "checkpoints":
-                        layer.objects.forEach(({ x, y }) => {
-                            customMap.checkpoints.push({
-                                x: Math.round(x),
-                                y: Math.round(y)
-                            })
-                        })
-                    break
-                }
+            if (layer.isObjectLayer && layer.name === "collision") {
+                layer.objects.forEach(obj => {
+                    const { x, y , width, height, properties } = obj
+                    customMapData.collisionRects.push(Object.assign(
+                        { x: Math.round(x), y: Math.round(y), width: Math.round(width), height: Math.round(height) },
+                        properties()
+                    ))
+                })
+                return
+            }
+            if (layer.isObjectLayer && layer.name === "checkpoints") {
+                layer.objects.forEach(({ x, y }) => {
+                    customMapData.checkpoints.push({
+                        x: Math.round(x),
+                        y: Math.round(y)
+                    })
+                })
+                return
+            }
+            const { width: mapWidth, height: mapHeight } = layer
+            const { zIndex=layerIndex, pFx=1, pFy=1, offsetX=0, offsetY=0 } = layer.properties()
+            const layerData = {
+                name: layer.name,
+                zIndex,
+                offsetX,
+                offsetY,
+                pFx,
+                pFy,
+                tiles: []
+            }
+            if (layer.isObjectLayer && layer.name === "spawn-points") { // spawn points
+                layer.objects.forEach(({ name, x, y, properties }) => {
+                    if (!name) { throw new Error("Anonymous spawn-point") }
+                    layerData.tiles.push(Object.assign(
+                        { name, x: Math.round(x), y: Math.round(y) },
+                        properties()
+                    ))
+                })
+                customMapData.layers.push(layerData)
                 return
             }
             // map layer
-            const { width: mapWidth, height: mapHeight } = layer
             const tilemap = Array(mapWidth * mapHeight).fill(0).map((_, i) => i)
-            const tilesArray = (layer.name === "foreground" && customMap.fgTiles) || 
-                                (layer.name === "midground" && customMap.mgTiles) ||
-                                (layer.name === "background" && customMap.bgTiles) ||    
-                                (layer.name === "far-background" && customMap.fbgTiles) ||    
-                                (layer.name === "world" && customMap.tiles)
-            if (!tilesArray) { return }                                
             tilemap.forEach(i => {
                 const column = i % mapWidth
                 const row = Math.floor(i / mapWidth)
@@ -78,20 +75,16 @@ const neutrinoFormatExtension = {
                 const altTileName = imageFileName.replace(/^.+\//g, "").replace(/\..+$/, "") // just filename without extension
                 props.name = props.name || altTileName
                 
-                tilesArray.push(Object.assign({
+                layerData.tiles.push(Object.assign({
                     x: column * tileWidth, 
                     y: (row + 1) * tileHeight - tileImageHeight, // computing the correct y value by taking offsets into account (changing it from y-coordinates of the tile at bottom-left corner of tileImage to the y-coordinates of top left-corner of the image) 
                 }, props))
             })
+            customMapData.layers.push(layerData)
         })
-        if (!customMap.bgTiles.length) {
-            delete customMap.bgTiles
-        }
-        if (!customMap.fbgTiles.length) {
-            delete customMap.fbgTiles
-        }
+        customMapData.layers.sort((a, b) => a.zIndex - b.zIndex)
         const outputFilename = fileName.replace(/\..+$/, ".cson") // making sure the output file extension is cson
-        const fileContent = JSON.stringify(customMap)
+        const fileContent = JSON.stringify(customMapData)
         const file = new TextFile(outputFilename, TextFile.WriteOnly)
         file.write(fileContent)
         file.commit()
